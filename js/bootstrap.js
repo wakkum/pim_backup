be865c51ee9ca566c62ab1a99aeea3e5ccd3398c
Yarno Ritzen <yarnoritzen@gmail.com>
2017-06-04 14:28:11 +0300

    updated text and image

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -334,7 +334,7 @@ if (typeof jQuery === 'undefined') {
   Carousel.TRANSITION_DURATION = 600
 
   Carousel.DEFAULTS = {
-    interval: 1700,
+    interval: 2000,
     pause: 'hover',
     wrap: true,
     keyboard: true
